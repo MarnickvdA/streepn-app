4ebbfa6a3f80f1383a973063bed005547857d57e
Marnick van der Arend <marnick@vdarend.com>
2021-03-10 20:48:06 +0100

    feat(profile): Editing your displayName supported

--- src/app/modules/profile/profile.page.ts
+++ src/app/modules/profile/profile.page.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import firebase from 'firebase/app';
-import {NavController} from '@ionic/angular';
+import {AlertController, LoadingController, NavController} from '@ionic/angular';
 import {AuthService} from '@core/services';
 import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
 import {faAt, faClock, faUser} from '@fortawesome/pro-duotone-svg-icons';
+import {TranslateService} from '@ngx-translate/core';
 import User = firebase.User;
 import Timestamp = firebase.firestore.Timestamp;
 
@@ -18,6 +19,9 @@ export class ProfilePage implements OnInit {
 
     constructor(private authService: AuthService,
                 private navController: NavController,
+                private alertController: AlertController,
+                private loadingController: LoadingController,
+                private translate: TranslateService,
                 private iconLibrary: FaIconLibrary) {
         this.iconLibrary.addIcons(faUser, faClock, faAt);
         this.user$ = this.authService.user;
@@ -29,5 +33,53 @@ export class ProfilePage implements OnInit {
 
     getTimestamp(time: string): Timestamp {
         return Timestamp.fromMillis(Date.parse(time));
+    }
+
+    async updateName(name: string) {
+        const alert = await this.alertController.create({
+            header: this.translate.instant('profile.edit.username'),
+            inputs: [
+                {
+                    name: 'username',
+                    type: 'text',
+                    value: name
+                },
+            ],
+            buttons: [
+                {
+                    text: this.translate.instant('actions.cancel'),
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                }, {
+                    text: this.translate.instant('actions.submit'),
+                    handler: async (data: { username: string }) => {
+                        if (data.username !== name && data.username.length > 2) {
+                            const loading = await this.loadingController.create({
+                                backdropDismiss: false,
+                                message: this.translate.instant('actions.updating')
+                            });
+
+                            loading.present();
+
+                            // TODO error handling
+                            this.authService.setName(data.username)
+                                .finally(() => {
+                                    loading.dismiss();
+                                });
+                        }
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    updateEmail() {
+
+    }
+
+    updatePassword() {
+
     }
 }
