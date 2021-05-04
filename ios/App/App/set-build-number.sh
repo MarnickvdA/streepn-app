a8e34adfbe31b38077cb4953c2a812ba2d14450a
Marnick van der Arend <m.vanderarend@strype.nl>
2021-05-04 15:02:47 +0200

    fix(build): Changed get version from package.json to get version from git tag

--- ios/App/App/set-build-number.sh
+++ ios/App/App/set-build-number.sh
@@ -7,8 +7,7 @@
 #  
 git=$(sh /etc/profile; which git)
 number_of_commits=$((`"$git" rev-list HEAD --count --first-parent`))
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-APP_VERSION=$(cat "${DIR}/../../../package.json" | grep version | sed "s/[\",]//g" | tr -d "[[:space:]]" | tr -d "version:")
+APP_VERSION=$((`"$git" describe --abbrev=0`))
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
