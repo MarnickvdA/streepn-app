e81fa09b5eb44c696f80007e1090ae4f68161ac7
Marnick van der Arend <m.vanderarend@strype.nl>
2021-08-03 15:01:37 +0200

    refactor(functions): Removed functions from this repository and moved it to separate repository

--- functions/src/models/auth-user.ts
+++
@@ -1,6 +0,0 @@
-export interface AuthUser {
-    uid: string;
-    displayName: string;
-    email: string;
-    photoURL: string;
-}
--- functions/src/models/stock.ts
+++
@@ -1,9 +0,0 @@
-export interface Stock {
-    id: string;
-    createdAt: any;
-    createdBy: string;
-    productId: string;
-    amount: number;
-    cost: number;
-    paidById: string;
-}
