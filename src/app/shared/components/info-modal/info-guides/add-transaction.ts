b058fccbe5f8e72e7de4d1b9c3fa49a084393879
Marnick van der Arend <m.vanderarend@strype.nl>
2021-08-03 19:46:15 +0200

    feat(info): Added information pages for several important pages

--- src/app/shared/components/info-modal/info-guides/add-transaction.ts
+++ src/app/shared/components/info-modal/info-guides/add-transaction.ts
@@ -1,1 +1,35 @@
-export const addTransactionGuide = ``;
+export const addTransactionGuide = `
+<p>
+    Op deze pagina kun je een nieuwe transactie toevoegen. Hieronder leggen we je precies uit wat er allemaal mogelijk is!
+</p>
+
+<h4>Product selecteren</h4>
+<p>
+    Standaard staat je eerste product in de lijst ingesteld als het geselecteerde product. Je kunt op de balk met 'Geselecteerd product' 
+    klikken om de lijst met andere producten te openen en daaruit een product te selecteren.
+</p>
+
+<h4>Streepjes zetten</h4>
+<p>
+    Je kunt voor je geselecteerde product op dat moment op de <strong>+</strong> en <strong>-</strong> knoppen drukken zodat je streepjes
+    kan toevoegen voor een gebruiker. 
+</p>
+<h6>Allemaal +1</h6>
+<p>
+    Je kunt voor het geselecteerde product gelijk alle deelnemers een streepje geven met de 'Allemaal +1' knop. 
+</p>
+<h6>Resetten</h6>
+<p>
+    Je kunt in een keer de gehele transactie resetten door op de 'Reset' knop te drukken.
+</p>
+
+<h4>Afrekenen</h4>
+<p>
+    Zodra je 1 of meerdere streepjes hebt gezet kun je de transactie afrekenen. Op dat moment zal het saldo van de gebruiker(s) op wie jij
+    gestreept hebt worden aangepast en wordt de voorraad ook veranderd op basis van jouw transactie. 
+</p>
+<p>
+    Mocht je op een later moment erachter komen dat je een foutje hebt gemaakt is het vaak nog mogelijk om je transactie aan te passen of 
+    ongedaan te maken.
+</p>
+`;
--- src/app/shared/components/info-modal/info-guides/stock.ts
+++ src/app/shared/components/info-modal/info-guides/stock.ts
@@ -3,22 +3,28 @@ export const stockPageGuide = `
 Op deze pagina kun je de producten in je huis beheren en voorraden aanvullen.
 </p>
 
+<h4>Tips voor je administratie!</h4>
 <p>
-<label>Tips voor jou!</label>
-<strong>Statiegeld: </strong> je kunt statiegeld toevoegen als normaal product. Dan houdt je beter overzicht over je huidige voorraad en de
-hoeveelheid statiegeld die je nog hebt. Je kunt bijvoorbeeld een statiegeld product met een prijs van 5 of 10 cent toevoegen. Voor de 
-meeste flesjes/blikjes etcetera kun je dan makkelijk statiegeld apart invoeren!
+    <strong>Statiegeld: </strong> je kunt statiegeld toevoegen als product. Hierdoor houdt je beter overzicht over de huidige voorraad en de
+    hoeveelheid statiegeld die er momenteel aanwezig is. Je kunt het 'statiegeld' product met een prijs van 5 of 10 cent toevoegen. 
+    Voor de meeste flesjes/blikjes kun je dan het statiegeld apart invoeren!
 </p>
 
-<h4>Producten</h4>
+<h4>Product toevoegen</h4>
 <p>
-- Toevoegen
-- Bewerken
+    Een product kan je toevoegen door op de 'Product toevoegen' knop te klikken. Kies een geschikte en duidelijke naam voor je product. 
+    Je kunt ook een kostprijs toevoegen aan het product, dit is echter niet perse nodig.
+</p>
+<p>
+    Als er voorraad is toegevoegd voor een product wordt automatisch een <strong>adviesprijs</strong> getoond in de detailpagina van het
+    product. Deze adviesprijs kun je gebruiken om een goede prijs te hanteren voor je producten. Het doel is immers om het verschil tussen
+    je kostprijs en inkoopprijs zo klein mogelijk te houden zodat de saldi niet een vertekend beeld geven.
 </p>
 
-<h4>Voorraad</h4>
+<h4>Voorraad aanvullen</h4>
 <p>
-- Toevoegen
-- Bewerken
+    Je kunt de saldi van deelnemers ophogen door voorraad toe te voegen aan een product. Dit is ook de enige manier om het saldo van een
+    deelnemer op te hogen. Selecteer een product, de hoeveelheid, de totale waarde (handig: statiegeld als apart product) en degene wiens 
+    saldo moet worden opgehoogd.
 </p>
 `;
